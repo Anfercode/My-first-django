766580911ef8120aa4f61514160339c845071a9d
Andres Campero <andesecci12@gmail.com>
2021-01-10 01:40:43 -0500

    Adding the model of users in post app

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,21 @@
+"""Post models"""
+
+#Django
 from django.db import models
 
 # Create your models here.
+
+class user(models.Model):
+    """ User Model """
+
+    uuid = models.UUIDField(unique=True)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    bio = models.TextField(blank=True)
+    birthdate = models.DateField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
