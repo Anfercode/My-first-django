aabcb1ff03e17533b1393e16b39d9539ae85a06b
Andres Campero <andesecci12@gmail.com>
2021-01-12 00:23:10 -0500

    Profile in-line admin for users.

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,11 @@
 """ User admin classes"""
 
 # Django
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib import admin
 
 # Models
+from django.contrib.auth.models import User
 from users.models import Profile
 
 
@@ -46,3 +48,49 @@ class ProfileAdmin(admin.ModelAdmin):
         'user__is_active',
         'user__is_staff',
     )
+
+    readonly_fields = ('created', 'modified')
+
+    fieldsets = (
+        ('Profile', {
+            'fields': (
+                'user',
+                'picture'
+            ),
+        }),
+        ('Extra info', {
+            'fields' : (
+                ('website', 'phone_number'),
+                ('biography')
+            )
+        }),
+        ('Metadata', {
+            'fields': (('created', 'modified'))
+        })
+    )
+
+
+class ProfileInline(admin.StackedInline):
+    """Profile in-line admin for users."""
+
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+
+class UserAdmin(BaseUserAdmin):
+    """Add profile admin to base user admin."""
+
+    inlines = (ProfileInline,)
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff'
+    )
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
