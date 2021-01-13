a0ea6af594b8b3d88812dd0dc38f2c16d5923366
Andres Campero <andesecci12@gmail.com>
2021-01-12 23:06:48 -0500

    Adding the post model and the static files config

--- posts/models.py
+++ posts/models.py
@@ -2,20 +2,18 @@
 
 #Django
 from django.db import models
+from django.contrib.auth.models import User
+from users.models import Profile
 
-# Create your models here.
+class Post(models.Model):
+    """ post Model """
+    profile =  models.ForeignKey(to=Profile, on_delete=models.CASCADE)
 
-class user(models.Model):
-    """ User Model """
+    title = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='posts/photos')
 
-    uuid = models.UUIDField(unique=True)
-    email = models.EmailField(max_length=100)
-    password = models.CharField(max_length=100)
-
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-
-    bio = models.TextField(blank=True)
-    birthdate = models.DateField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.title} - {self.profile.user.username}'
